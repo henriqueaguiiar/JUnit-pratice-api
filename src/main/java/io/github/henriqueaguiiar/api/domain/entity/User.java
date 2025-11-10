226d21ba1b1d27bb94db5b623bc9240afd7c09d0
henriqueaguiiar <henriqueaguiarpacheco09@gmail.com>
2025-11-10 19:53:32 -0300

    feat: test profile implements. application-test.properties, infrastructure.config/LocalConfig.java

--- src/main/java/io/github/henriqueaguiiar/api/domain/entity/User.java
+++ src/main/java/io/github/henriqueaguiiar/api/domain/entity/User.java
@@ -7,6 +7,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.Objects;
 
 @Data
@@ -14,7 +15,8 @@ import java.util.Objects;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity(name = "tb_user")
-public class User {
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
