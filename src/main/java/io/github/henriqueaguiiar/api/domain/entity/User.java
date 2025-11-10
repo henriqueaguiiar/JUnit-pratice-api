243334da1b0cf2ebfff8f0d852baab4abb88e369
henriqueaguiiar <henriqueaguiarpacheco09@gmail.com>
2025-11-10 18:40:24 -0300

    feat: Entity User

---
+++ src/main/java/io/github/henriqueaguiiar/api/domain/entity/User.java
@@ -0,0 +1,27 @@
+package io.github.henriqueaguiiar.api.domain.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+}
