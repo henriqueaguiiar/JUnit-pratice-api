88a6bf00d82a93e4310311f5afa7f542fd49fd8b
henriqueaguiiar <henriqueaguiarpacheco09@gmail.com>
2025-11-10 20:43:32 -0300

    feat UserDTO entity

---
+++ src/main/java/io/github/henriqueaguiiar/api/domain/dto/UserDTO.java
@@ -0,0 +1,18 @@
+package io.github.henriqueaguiiar.api.domain.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+
+}
